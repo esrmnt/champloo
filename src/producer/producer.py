0b17c3249413f63cd8d62c4e7e23f6ee2eb1a975
esrmnt <nitish.er@gmail.com>
2024-06-09 17:10:01 +0530

    Added rudemantry producer

---
+++ src/producer/producer.py
@@ -0,0 +1,28 @@
+import json
+import logging as log
+
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+# Create a producer with JSON serializer
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# Sending JSON data
+future = producer.send('monthly-report', {'field': 'value'})
+# Ensure all messages are sent before exiting
+producer.flush()
+
+# Block for 'synchronous' sends
+try:
+    record_metadata = future.get(timeout=10)
+except KafkaError:
+    # Decide what to do if produce request failed...
+    log.exception()
+    pass
+# Successful result returns assigned partition and offset
+print (record_metadata.topic)
+print (record_metadata.partition)
+print (record_metadata.offset)
